0277e08679fbe512f77dcb23028c70b1ee31a619
channu salgar <channu.chincholi@gmail.com>
2019-09-20 19:06:04 +0530

    added search page feature code

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -1,7 +1,12 @@
 package com.qa.pages;
 
 public class LoginPage {
-public void loginpage() {
+
+	public void loginpage() {
 	System.out.println("login page");
 }
+
+	public void loginset() {
+		System.out.println("login set");
+	}
 }
